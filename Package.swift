3524f8632785b03d5d05b25a2980e9e4098c5772
Hung Ling Ho <carl.hung@yahoo.com>
2020-11-27 14:23:34 +0800

    added support os

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "NavigationCacheController",
+    platforms: [
+        .iOS(SupportedPlatform.IOSVersion.v9),
+        .macOS(SupportedPlatform.MacOSVersion.v10_13),
+        .tvOS(SupportedPlatform.TVOSVersion.v9),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
